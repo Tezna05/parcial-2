dee34fe5ac5b41a5b7468a18c16659e3f59c1054
Tezna05 <juantezna89@gmail.com>
2024-09-18 11:05:03 -0500

    Update parcial1-3.py

---
+++ parcial1-3.py
@@ -0,0 +1,7 @@
+texto=input("inglese una cadena de texto")
+text_mayus=texto.upper()
+longitud=len
+conting_python="python" in texto
+print(f"texto en mayusculas:{text_mayus}")
+print(f"numero caracteres:{longitud}")
+print(f"contiene la palabra python:{conting_python}")
